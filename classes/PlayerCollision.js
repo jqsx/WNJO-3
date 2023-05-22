3ba9b9a774efef8d25fd788400d4ecfa6488ebb6
jqsx <101830653+jqsx@users.noreply.github.com>
2023-05-22 17:09:07 +0200

    bing

--- classes/PlayerCollision.js
+++ classes/PlayerCollision.js
@@ -17,24 +17,45 @@ export class PlayerCollision {
             let localPlayer = this.app.localPlayer;
             let campos = this.app.cameraPosition;
             let wbpos = col.chunk.chunkPosition.multiply(256).add(col.wb.position).add(new vec(col.wb.scale.x / 2, col.wb.scale.y / 2));
-            let diff = wbpos.add(new vec(-col.wb.scale.x / 2, col.wb.scale.y / 2)).sub(localPlayer.position)
+            // let diff = wbpos.add(new vec(-col.wb.scale.x / 2, col.wb.scale.y / 2)).sub(localPlayer.position);
+            let wbCentered = wbpos.add(new vec(-col.wb.scale.x / 2, col.wb.scale.y / 2));
 
-            if (Math.abs(diff.x) > Math.abs(diff.y)) {
-                this.app.cameraPosition.y -= diff.y;
-            }
-            else {
-                this.app.cameraPosition.x -= diff.x;
+            let diff = wbCentered.sub(localPlayer.position);
+            let gap = diff.sub(new vec(-col.wb.scale.x / 2, col.wb.scale.y / 2));
+
+            const determineDisplacement = (gap, playerDimension, targetDimension) => {
+                let min = gap;
+
+                const other = gap + playerDimension + targetDimension;
+                if (other < Math.abs(min)) min = other;
+
+                return min;
+            };
+
+            let displacement = new vec(determineDisplacement(gap.x, 7, col.wb.scale.x), determineDisplacement(gap.y, 11, col.wb.scale.y));
+            DEBUG.log("bing: " + displacement);
+            if (Math.abs(displacement.y) > Math.abs(displacement.x)) {
+                localPlayer.position.y -= displacement.y / 2;
+                campos.y = localPlayer.position.y;
+                DEBUG.log("disp y");
+            } else {
+                DEBUG.log("disp x");
+                localPlayer.position.x -= displacement.x / 2;
+                campos.x = localPlayer.position.x;
             }
         }
     }
 
     collisionCheck() {
         let localPlayer = this.app.localPlayer;
-        let chunkPos = new vec(Math.floor(localPlayer.position.x / (256)), Math.floor(localPlayer.position.y / (256))); 
+        let chunkPos = new vec(
+            Math.floor(localPlayer.position.x / 256),
+            Math.floor(localPlayer.position.y / 256)
+        );
         var dir = new vec(chunkPos.x * 256 + 128, chunkPos.y * 256 + 128);
         const ooftSign = (n) => {
             return n > 0 ? 1 : -1;
-        }
+        };
 
         dir.x -= localPlayer.position.x;
         dir.y -= localPlayer.position.y;
@@ -46,7 +67,14 @@ export class PlayerCollision {
                     for (let i = 0; i < _chank.worldBlocks.length; i++) {
                         const wb = _chank.worldBlocks[i];
                         if (!wb.isSolid) continue;
-                        if (localPlayer.position.distance(_chank.chunkPosition.multiply(256).add(new vec(wb.position.x + 8, wb.position.y + 8))) < 16 + Math.max(wb.scale.x, wb.scale.y)) {
+                        if (
+                            localPlayer.position.distance(
+                                _chank.chunkPosition
+                                    .multiply(256)
+                                    .add(new vec(wb.position.x + 8, wb.position.y + 8))
+                            ) <
+                            16 + Math.max(wb.scale.x, wb.scale.y)
+                        ) {
                             if (this.rectIntersect(wb, _chank)) {
                                 return { wb: wb, chunk: _chank };
                             }
@@ -63,14 +91,16 @@ export class PlayerCollision {
         let localPlayer = this.app.localPlayer;
 
         let chunkWorldPos = chunk.chunkPosition.multiply(256);
-        let actual = new vec(worldBlock.position.x ,worldBlock.position.y).add(chunkWorldPos);
+        let actual = new vec(worldBlock.position.x, worldBlock.position.y).add(
+            chunkWorldPos
+        );
         // DEBUG.debugPoint(actual);
         // DEBUG.debugPoint(test);
         // DEBUG.debugPoint(localPlayer.position);
 
         // let relativeToChunk = localPlayer.position.sub(chunk.chunkPosition.multiply(256));
         // if (relativeToChunk.x < -35 || relativeToChunk.x > 256 + 35 + worldBlock.scale.x || relativeToChunk.y < -55 || relativeToChunk.y > 256 + 55 + worldBlock.scale.y) return false;
-        
+
         let a = localPlayer.position;
         let b = actual.add(new vec(0, 0));
 
